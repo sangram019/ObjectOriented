058b65fa07a8afe5b9a711c92e5e21a0dabbaadc
Sangram <sangram15.behera@gmail.com>
2022-05-16 03:05:17 +0530

    Added By Sangram

--- ObjectOriented/Program.cs
+++ ObjectOriented/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using OOPS.InventoryDataManagement;
+using System;
+
+namespace OOPS
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------Welcome To Inventory Management-----------");
+            const string INVENTORY_DATA_PATH = @"C:\Users\HP\ObjectOrientedPrograms\ObjectOrientedPrograms\ObjectOrientedPrograms\Inventory.json";
+            InventoryManagementFile inventory = new InventoryManagementFile();
+            Console.WriteLine("Select Number\n1.Display\n");
+            bool check = true;
+            while (check)
+            {
+                Console.WriteLine("Enter The Option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        inventory.DisplayInventory(INVENTORY_DATA_PATH);
+                        break;
+                    case 2:
+                        check = false;
+                        break;
+                        ;
+                }
+            }
+        }
+    }
+
+}
